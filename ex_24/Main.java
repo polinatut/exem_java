28bd5c182540c2be630b70978c1c0e69fa7871cf
polinatuteva <73732707+polinatut@users.noreply.github.com>
2025-01-26 21:43:30 +0300

    Update Main.java

--- ex_24/Main.java
+++ ex_24/Main.java
@@ -1,17 +1,10 @@
-
-// Демонстрация работы классов
 public class Main {
     public static void main(String[] args) {
         SmartForm smartForm = new SmartForm();
 
-        // Ввод текста и пароля
         smartForm.inputText();
         smartForm.inputPassword();
-
-        // Отправка данных
         smartForm.submit();
-
-        // Отображение сохраненного пароля
         smartForm.displayPassword();
     }
 }
