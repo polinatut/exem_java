0c4f95e28b69ba2414b0b3716edff0af06167338
polinatuteva <73732707+polinatut@users.noreply.github.com>
2025-01-26 21:42:55 +0300

    Update Form.java

--- ex_24/Form.java
+++ ex_24/Form.java
@@ -1,18 +1,14 @@
-
 import java.util.Scanner;
 
-// Базовый класс Form
 class Form {
     protected String textInput;
 
-    // Метод для ввода текста
     public void inputText() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите текст: ");
         textInput = scanner.nextLine();
     }
 
-    // Метод для отправки текста
     public void submit() {
         System.out.println("Текст отправлен: " + textInput);
     }
