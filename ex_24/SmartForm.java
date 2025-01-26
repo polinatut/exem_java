07c50eca9271fa234ebe53251473b9ae3a337f78
polinatuteva <73732707+polinatut@users.noreply.github.com>
2025-01-26 21:43:52 +0300

    Update SmartForm.java

--- ex_24/SmartForm.java
+++ ex_24/SmartForm.java
@@ -1,21 +1,17 @@
-
 import java.util.Scanner;
 class SmartForm extends Form {
     private String password;
 
-    // Метод для ввода пароля
     public void inputPassword() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите пароль: ");
         password = scanner.nextLine();
     }
 
-    // Метод для проверки и отображения пароля
     public void displayPassword() {
         System.out.println("Сохранённый пароль: " + password);
     }
 
-    // Переопределение метода submit для отображения текста и пароля
     @Override
     public void submit() {
         System.out.println("Текст отправлен: " + textInput);
